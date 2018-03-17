77eb3fc32cf52505a34e1d5826778eea012cfb45
Kamil <kamilsmas@gmail.com>
2018-03-17 22:00:25 +0100

    add method for gallery images

---
+++ app/src/main/java/com/example/android/wishlist/WishAdapter.java
@@ -0,0 +1,10 @@
+package com.example.android.wishlist;
+
+/**
+ * Created by Kamil on 2018-03-17.
+ */
+
+public class WishAdapter  {
+
+
+}
--- app/src/main/java/com/example/android/wishlist/WishAddActivity.java
+++ app/src/main/java/com/example/android/wishlist/WishAddActivity.java
@@ -1,6 +1,32 @@
 package com.example.android.wishlist;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.realm.Realm;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
 
 /**
  * Created by Kamil on 2018-03-17.
@@ -8,8 +34,81 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WishAddActivity extends AppCompatActivity {
 
+    @BindView(R.id.nameEditText) EditText nameEditText;
+    @BindView(R.id.contentEditText) EditText contentEditText;
+    @BindView(R.id.imageView) ImageView imageView;
 
 
+    private Wish wish;
+    private Realm realm;
+    private byte[] arrPhoto;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wish_add);
+        ButterKnife.bind(this);
+        realm.init(this);
+        realm = Realm.getDefaultInstance();
+
+    }
+
+
+    @OnClick(R.id.fab) public void onClickSave() {
+        String content = contentEditText.getText().toString();
+        Person person = new Person(nameEditText.getText().toString());
+        wish = new Wish(content, person, arrPhoto);
+        realm.beginTransaction();
+        realm.copyToRealm(wish);
+        realm.commitTransaction();
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+        WishAddActivity.this.finish();
+
+    }
+
+    @OnClick(R.id.buttonAddPhoto) public void onClickAddPhoto() {
+        if(ContextCompat.checkSelfPermission(WishAddActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(WishAddActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 123);
+
+        } else {
+            EasyImage.openChooserWithGallery(WishAddActivity.this, "Select source", 0);
+
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        EasyImage.handleActivityResult(requestCode, resultCode, data, this, new DefaultCallback() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+                Toast.makeText(WishAddActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 70, stream);
+                arrPhoto = stream.toByteArray();
+                if(imageView != null) {
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView.setImageBitmap(bitmap);
+                }
+
+            }
+
+
+        });
+    }
 
 
 }
